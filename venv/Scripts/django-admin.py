95add87c7bc99bce18fca8036df09f20021f2b49
Chetana <kl98735@umbc.edu>
2020-04-08 17:36:59 -0400

    v 1

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Family\PycharmProjects\cmsc291-sp2020-assignment-4-kl98735\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
