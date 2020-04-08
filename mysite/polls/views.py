dbcd0539a9666d15c9aa4cbbab348db41b05b551
Chetana <kl98735@umbc.edu>
2020-04-08 17:53:38 -0400

    v 2

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 from .models import Question
@@ -20,9 +21,19 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = ', '.join([q.question_text for q in latest_question_list])
     template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
     return HttpResponse(template.render(context, request))
 
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+
+# ...
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
